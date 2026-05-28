0b863399b1e9d8dd2be432bb2a2b986a41d0a385
Ezra Duncan <etduncan03@gmail.com>
2026-05-28 12:10:59 +0100

    made singletons using eager and static blocks

---
+++ DesignPatterns/src/main/java/designPatterns/singleton/SingletonEager.java
@@ -0,0 +1,14 @@
+package designPatterns.singleton;
+
+public class SingletonEager {
+    private static SingletonEager eagerInstance = new SingletonEager();
+
+    private SingletonEager(){
+
+    }
+
+    public static SingletonEager getEagerInstance(){
+        return eagerInstance;
+    }
+
+}
---
+++ DesignPatterns/src/main/java/designPatterns/singleton/SingletonStaticBlock.java
@@ -0,0 +1,18 @@
+package designPatterns.singleton;
+
+public class SingletonStaticBlock {
+    private static SingletonStaticBlock instance;
+
+    static {
+        instance = new SingletonStaticBlock();
+    }
+
+    private SingletonStaticBlock(){
+
+    }
+
+    public static SingletonStaticBlock getInstance(){
+        return instance;
+    }
+
+}
